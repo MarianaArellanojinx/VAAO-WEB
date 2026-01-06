88ef8d062a9b3099b7eca014c44f803cda2481fd
Gerardo Chávez <gerardo_rodrigo.chavez@daimlertruck.com>
2026-01-06 17:45:00 -0600

    feat

--- vaao/src/app/features/home/home.component.ts
+++ vaao/src/app/features/home/home.component.ts
@@ -1,7 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card'
+import { AuthService } from '../../core/services/auth.service';
+import { User } from '../../shared/interfaces/User';
+import { RoleConst } from '../../shared/const/RoleConst';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +16,17 @@ import { CardModule } from 'primeng/card'
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   private router: Router = inject(Router);
+  private readonly auth: AuthService = inject(AuthService);
+
+  user!: User | null;
+  roles: any = RoleConst;
+
+  ngOnInit(): void {
+    this.user = this.auth.getUser();
+  }
 
   navigate(url:string) {
     this.router.navigate([url]);
---
+++ vaao/src/app/shared/const/RoleConst.ts
@@ -0,0 +1,6 @@
+export const RoleConst = {
+    ADMIN: 1,
+    ENCARGADO: 2,
+    REPARTIDOR: 3,
+    CLIENTE: 4
+}
