2603594f605b63e1636720d6595287c6514baa0e
Gerardo3Carcamo <gcarcamo@uadec.edu.mx>
2026-01-12 23:28:32 -0600

    ADD VIEW PHOTOS

--- vaao/src/app/features/admin/corte/corte.component.ts
+++ vaao/src/app/features/admin/corte/corte.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { TableModule } from "primeng/table";
+import { CardDashboardComponent } from "../../../shared/components/card-dashboard/card-dashboard.component";
+import { CommonModule } from '@angular/common';
+import { CalendarModule } from "primeng/calendar";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-corte',
   standalone: true,
-  imports: [],
+  imports: [TableModule, CommonModule, CalendarModule, FormsModule],
   templateUrl: './corte.component.html',
   styleUrl: './corte.component.scss'
 })
 export class CorteComponent {
+
+  fechaSeleccionada!: Date;
+
+  ventas = [
+    {
+      negocio: 'Tecate Six Pirámide',
+      cliente: 'Diana Solís',
+      hora: '10:35',
+      metodoPago: 'Efectivo',
+      importe: 350
+    }
+  ];
 
 }
